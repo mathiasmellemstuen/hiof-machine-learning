62e481ea2b61d2c02e1c17fc59da3a898b72fb54
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2021-09-23 22:28:30 +0200

    Done task 1 coding part. Started on task 2.

--- assignment_1/task_1.py
+++ assignment_1/task_1.py
@@ -1,37 +1,48 @@
 # Assignment 1
-# Mathias Mellemstuen
 # Task 1
+# Linear Regression | Food Truck
+# Mathias Mellemstuen
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plot
-
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 
 # Reading data from truckdata file
-data = pd.read_csv("truckdata.txt")
+data = pd.read_csv("truckdata.txt", header=None)
 
 # Splitting the datasets two columns into two variables, x and y. 
-# X: Predictor
-# Y: Response
 x = data.iloc[:,0]
 y = data.iloc[:,1]
+
+x = x.to_numpy()
+y = y.to_numpy()
 
 # Splitting the dataset into a training set and a test set. 
 testSetPercentage = 0.2
 trainingX, testX, trainingY, testY = train_test_split(x, y, test_size=testSetPercentage)
+trainingX = trainingX.reshape(-1, 1)
+trainingY = trainingY.reshape(-1, 1)
 
 linearRegression = linear_model.LinearRegression()
-linearRegression.fit([trainingX], [trainingY])
+linearRegression.fit(trainingX, trainingY)
 
 m = linearRegression.coef_
 b = linearRegression.intercept_
 
-#y = m * x + b
+y = m * trainingX + b
 
-plot.scatter(trainingX, trainingY)
+plot.plot([min(trainingX), max(trainingX)], [min(y), max(y)], linewidth=2, color='orange')
 
-line = m * [trainingX] + b
-#plot.scatter(x, y)
+# Showing training data (in blue) and test data (in green)
+plot.scatter(trainingX, trainingY, color = 'blue')
+plot.scatter(testX, testY, color = 'red')
+
+# Displaying legend text
+plot.legend(["Regression function", "Training data", "Test data"])
+
+plot.title("Linear regression: Truck data")
 plot.show()
+
+

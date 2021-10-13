74e02c60a5b8a524b4c97cbc65501441a51563e6
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2021-10-13 12:37:20 +0200

    Done coding of assignment 2

--- assignment_1/task_1.py
+++ assignment_1/task_1.py
@@ -5,7 +5,7 @@
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plot
+import matpleeotlib.pyplot as plot
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 
